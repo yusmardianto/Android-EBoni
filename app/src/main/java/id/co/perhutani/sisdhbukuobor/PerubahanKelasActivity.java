4cb08f606b3091690c08040bd823388650421526
Bagus Nurcahyo Putra <bagusnurcahyoputra@gmail.com>
2019-11-25 05:47:27 +0700

    perubahan kelas

---
+++ app/src/main/java/id/co/perhutani/sisdhbukuobor/PerubahanKelasActivity.java
@@ -0,0 +1,21 @@
+package id.co.perhutani.sisdhbukuobor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.co.perhutani.sisdhbukuobor.ui.perubahankelas.PerubahanKelasFragment;
+
+public class PerubahanKelasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.perubahan_kelas_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, PerubahanKelasFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
