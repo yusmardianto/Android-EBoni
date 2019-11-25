553918dd349b5be5e5c72297878476f799bacea6
Bagus Nurcahyo Putra <bagusnurcahyoputra@gmail.com>
2019-11-26 04:04:33 +0700

    list & tambah perubahan kelas

---
+++ app/src/main/java/id/co/perhutani/sisdhbukuobor/ui/perubahankelas/tambahperubahan/TambahPerubahanFragment.java
@@ -0,0 +1,43 @@
+package id.co.perhutani.sisdhbukuobor.ui.perubahankelas.tambahperubahan;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.co.perhutani.sisdhbukuobor.R;
+
+public class TambahPerubahanFragment extends Fragment {
+
+    private TambahPerubahanViewModel mViewModel;
+
+    public static TambahPerubahanFragment newInstance() {
+        return new TambahPerubahanFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+//        return inflater.inflate(R.layout.tambah_perubahan_fragment, container, false);
+
+        mViewModel = ViewModelProviders.of(this).get(TambahPerubahanViewModel.class);
+        View root = inflater.inflate(R.layout.tambah_perubahan_fragment, container, false);
+
+        return root;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(TambahPerubahanViewModel.class);
+        // TODO: Use the ViewModel
+    }
+}
---
+++ app/src/main/java/id/co/perhutani/sisdhbukuobor/ui/perubahankelas/tambahperubahan/TambahPerubahanViewModel.java
@@ -0,0 +1,7 @@
+package id.co.perhutani.sisdhbukuobor.ui.perubahankelas.tambahperubahan;
+
+import androidx.lifecycle.ViewModel;
+
+public class TambahPerubahanViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
