68cdbe12198c483232a24e0cdf32dee66584578f
Yusmardianto <yusmardianto1@gmail.com>
2019-11-27 01:47:17 +0700

    fix all menu

--- app/src/main/java/id/co/perhutani/sisdhbukuobor/ui/laporanpalbatas/ListPelaporanpalFragment.java
+++ app/src/main/java/id/co/perhutani/sisdhbukuobor/ui/laporanpalbatas/ListPelaporanpalFragment.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -16,6 +19,7 @@ import java.util.ArrayList;
 import id.co.perhutani.sisdhbukuobor.Adapter.PelaporanpalbatasAdapter;
 import id.co.perhutani.sisdhbukuobor.Model.PelaporanpalbatasModel;
 import id.co.perhutani.sisdhbukuobor.R;
+import id.co.perhutani.sisdhbukuobor.ui.laporanpalbatas.ui.tambahlaporanpalbatas.TambahlaporanpalbatasFragment;
 
 public class ListPelaporanpalFragment extends Fragment
 {
@@ -40,6 +44,18 @@ public class ListPelaporanpalFragment extends Fragment
 //        myrecyclerview.addItemDecoration(new VerticalSpaceItemDecoration(VERTICAL_ITEM_SPACE));
         myrecylcerview .setAdapter(gAdapter);
 
+        ImageView imgTambahPelaporanpal = (ImageView) root.findViewById(R.id.img_tambahlaporanpal);
+        imgTambahPelaporanpal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new TambahlaporanpalbatasFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.nav_host_fragment, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
         return root;
     }
 
---
+++ app/src/main/java/id/co/perhutani/sisdhbukuobor/ui/laporanpalbatas/ui/tambahlaporanpalbatas/TambahlaporanpalbatasViewModel.java
@@ -0,0 +1,7 @@
+package id.co.perhutani.sisdhbukuobor.ui.laporanpalbatas.ui.tambahlaporanpalbatas;
+
+import androidx.lifecycle.ViewModel;
+
+public class TambahlaporanpalbatasViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
