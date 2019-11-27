81921c246281103a878a472bbcee809a7363f24c
Yusmardianto <yusmardianto1@gmail.com>
2019-11-27 20:48:34 +0700

    create perubahankelas

---
+++ app/src/main/java/id/co/perhutani/sisdhbukuobor/Schema/TrnPerubahanKelas.java
@@ -0,0 +1,58 @@
+package id.co.perhutani.sisdhbukuobor.Schema;
+
+public class TrnPerubahanKelas {
+    public static final String TABLE_NAME = "TRN_PERUBAHAN_KELAS";
+    public static final String _ID = "ID";
+    public static final String ANAK_PETAK_ID_PERUBAHAN = "PETAK_ID";
+    public static final String TAHUN_PERUBAHAN = "TAHUN";
+    public static final String LUAS_PERUBAHAN = "LUAS";
+    public static final String JENIS_TANAMAN_PERUBAHAN = "JENIS_TANAMAN";
+    public static final String KELAS_HUTAN_PERUBAHAN = "KELAS_HUTAN";
+    public static final String LUAS_PERKIRAAN = "LUAS_PERKIRAAN";
+    public static final String JENIS_TANAMAN_PERKIRAAN = "JENIS_TANAMAN_PERKIRAAN";
+    public static final String KELAS_HUTAN_PERKIRAAN = "KELAS_HUTAN_PERKIRAAN";
+    public static final String NO_BAPPKH = "NO_BAPPKH";
+    public static final String LUAS_DEFINITIF = "LUAS_DEFINITIF";
+    public static final String JENIS_TANAMAN_DEFINITIF = "JENIS_TANAMAN_DEFINITIF";
+    public static final String KELAS_HUTAN_DEFINITIF = "KELAS_HUTAN_DEFINITIF";
+    public static final String KETERANGAN_PERUBAHAN = "KETERANGAN";
+    public static final String KET1 = "KET1";
+    public static final String KET2 = "KET2";
+    public static final String KET3 = "KET3";
+    public static final String KET4 = "KET4";
+    public static final String KET5 = "KET5";
+    public static final String KET6 = "KET6";
+    public static final String KET7 = "KET7";
+    public static final String KET8 = "KET8";
+    public static final String KET9 = "KET9";
+    public static final String KET10 = "KET10";
+    public static final String SQL_CREATE_ENTRIES = "CREATE TABLE " +
+            "IF NOT EXISTS "+ TABLE_NAME +" ( "+ _ID +" INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "" + ANAK_PETAK_ID_PERUBAHAN + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + TAHUN_PERUBAHAN + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + LUAS_PERUBAHAN + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + JENIS_TANAMAN_PERUBAHAN + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KELAS_HUTAN_PERUBAHAN + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + LUAS_PERKIRAAN + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + JENIS_TANAMAN_PERKIRAAN + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KELAS_HUTAN_PERKIRAAN + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + NO_BAPPKH + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + LUAS_DEFINITIF + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + JENIS_TANAMAN_DEFINITIF + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KELAS_HUTAN_DEFINITIF + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KETERANGAN_PERUBAHAN + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KET1 + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KET2 + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KET3 + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KET4 + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KET5 + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KET6 + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KET7 + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KET8 + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KET9 + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KET10 + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + Type.CREATED_AT_CURRENT_DATE + ", " +
+            "" + Type.UPDATED_AT_CURRENT_DATE + " )";
+    public static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    public static final String SQL_DELETE_ALL_ROWS = "DELETE FROM " + TABLE_NAME + " WHERE " + _ID + " <> 0";
+}
--- app/src/main/java/id/co/perhutani/sisdhbukuobor/ui/perubahankelas/tambahperubahan/TambahPerubahanFragment.java
+++ app/src/main/java/id/co/perhutani/sisdhbukuobor/ui/perubahankelas/tambahperubahan/TambahPerubahanFragment.java
@@ -1,22 +1,38 @@
 package id.co.perhutani.sisdhbukuobor.ui.perubahankelas.tambahperubahan;
 
-import androidx.lifecycle.ViewModelProviders;
-
+import android.content.ContentValues;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
+import id.co.perhutani.sisdhbukuobor.ExtentionClass.AjnClass;
+import id.co.perhutani.sisdhbukuobor.ExtentionClass.SQLiteHandler;
 import id.co.perhutani.sisdhbukuobor.R;
+import id.co.perhutani.sisdhbukuobor.Schema.TrnPerubahanKelas;
 
 public class TambahPerubahanFragment extends Fragment {
 
+    private EditText petak_id, tahun, luas, jenis_tanaman, kelas_tanaman, luas_perkiraan,
+            jenis_perkiraan, kelas_perkiraan, no_bappkh, luas_definitif, jenis_definitif,
+            kelas_definitif, keterangan;
+
+
     private TambahPerubahanViewModel mViewModel;
+
+    private static SQLiteHandler db;
+
+    private Button btnSimpanPerubahan;
+
 
     public static TambahPerubahanFragment newInstance() {
         return new TambahPerubahanFragment();
@@ -26,13 +42,64 @@ public class TambahPerubahanFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tambah_perubahan_fragment, container, false);
-    }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(TambahPerubahanViewModel.class);
-        // TODO: Use the ViewModel
+        View root = inflater.inflate(R.layout.tambah_perubahan_fragment, container, false);
+
+        db = new SQLiteHandler(getActivity());
+
+        petak_id = root.findViewById(R.id.perubahankls_idpetakperubahan);
+        tahun = root.findViewById(R.id.perubahankls_tahunperubahan);
+        luas = root.findViewById(R.id.perubahankls_luasperubahan);
+        jenis_tanaman = root.findViewById(R.id.perubahankls_jenisperubahan);
+        kelas_tanaman = root.findViewById(R.id.perubahankls_kelasperubahan);
+        luas_perkiraan = root.findViewById(R.id.perubahankls_luasperkiraan);
+        jenis_perkiraan = root.findViewById(R.id.perubahankls_jenisperkiraan);
+        kelas_perkiraan = root.findViewById(R.id.perubahankls_kelasperkiraan);
+        no_bappkh = root.findViewById(R.id.perubahankls_nobappkh);
+        luas_definitif = root.findViewById(R.id.perubahankls_luasdefinitif);
+        jenis_definitif = root.findViewById(R.id.perubahankls_jenisdefinitif);
+        kelas_definitif = root.findViewById(R.id.perubahankls_kelasdefinitif);
+        keterangan = root.findViewById(R.id.perubahankls_ketperubahan);
+        btnSimpanPerubahan = root.findViewById(R.id.perubahankls_btnsubmit);
+
+        btnSimpanPerubahan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try
+                {
+                    ContentValues values_aktifitas = new ContentValues();
+                    values_aktifitas.put(TrnPerubahanKelas.ANAK_PETAK_ID_PERUBAHAN, petak_id.getText().toString());
+                    values_aktifitas.put(TrnPerubahanKelas.TAHUN_PERUBAHAN, tahun.getText().toString());
+                    values_aktifitas.put(TrnPerubahanKelas.LUAS_PERUBAHAN, luas.getText().toString());
+                    values_aktifitas.put(TrnPerubahanKelas.JENIS_TANAMAN_PERUBAHAN, jenis_tanaman.getText().toString());
+                    values_aktifitas.put(TrnPerubahanKelas.KELAS_HUTAN_PERUBAHAN, kelas_tanaman.getText().toString());
+                    values_aktifitas.put(TrnPerubahanKelas.LUAS_PERKIRAAN, luas_perkiraan.getText().toString());
+                    values_aktifitas.put(TrnPerubahanKelas.JENIS_TANAMAN_PERKIRAAN, jenis_perkiraan.getText().toString());
+                    values_aktifitas.put(TrnPerubahanKelas.KELAS_HUTAN_PERKIRAAN, kelas_perkiraan.getText().toString());
+                    values_aktifitas.put(TrnPerubahanKelas.NO_BAPPKH, no_bappkh.getText().toString());
+                    values_aktifitas.put(TrnPerubahanKelas.LUAS_DEFINITIF, luas_definitif.getText().toString());
+                    values_aktifitas.put(TrnPerubahanKelas.JENIS_TANAMAN_DEFINITIF, jenis_definitif.getText().toString());
+                    values_aktifitas.put(TrnPerubahanKelas.KELAS_HUTAN_DEFINITIF, kelas_definitif.getText().toString());
+                    values_aktifitas.put(TrnPerubahanKelas.KETERANGAN_PERUBAHAN, keterangan.getText().toString());
+                    db.create(TrnPerubahanKelas.TABLE_NAME, values_aktifitas);
+                    Toast.makeText(getActivity(), "Data Berhasil Ditambah! ", Toast.LENGTH_SHORT).show();
+
+//                    // Move to fragment gangguan
+                    FragmentManager manager = (getActivity()).getSupportFragmentManager();
+                    Fragment fragment = new ListPerubahanKelasFragment();
+                    FragmentTransaction ft = manager.beginTransaction();
+                    ft.replace(R.id.nav_host_fragment, fragment);
+                    ft.commit();
+
+
+                } catch (Exception e)
+                {
+                    AjnClass.showAlert(getActivity(),e.toString());
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        return root;
     }
 }
