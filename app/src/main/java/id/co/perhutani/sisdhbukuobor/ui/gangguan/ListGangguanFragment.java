8efad4bc4de7b97939ac9d60b87731618f39645b
Adhi JN <adijoyonegoro@gmail.com>
2019-11-27 15:50:38 +0700

    update list view

--- app/src/main/java/id/co/perhutani/sisdhbukuobor/ui/gangguan/ListGangguanFragment.java
+++ app/src/main/java/id/co/perhutani/sisdhbukuobor/ui/gangguan/ListGangguanFragment.java
@@ -17,12 +17,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import id.co.perhutani.sisdhbukuobor.Adapter.GangguanAdapter;
 import id.co.perhutani.sisdhbukuobor.ExtentionClass.AjnClass;
 import id.co.perhutani.sisdhbukuobor.ExtentionClass.SQLiteHandler;
 import id.co.perhutani.sisdhbukuobor.Model.GangguanModel;
 import id.co.perhutani.sisdhbukuobor.R;
+import id.co.perhutani.sisdhbukuobor.ui.VerticalSpaceItemDecoration;
 import id.co.perhutani.sisdhbukuobor.ui.gangguan.tambahgangguan.TambahGangguanFragment;
 
 public class ListGangguanFragment extends Fragment
@@ -30,6 +32,8 @@ public class ListGangguanFragment extends Fragment
 //    View v ;
     private RecyclerView myrecyclerview;
     private ArrayList<GangguanModel> dataModels;
+    private List<GangguanModel> lsgangguan;
+    GangguanAdapter gAdapter;
 
     private static final int VERTICAL_ITEM_SPACE = 0;
     public static ListGangguanFragment newInstance() { return new ListGangguanFragment(); }
@@ -41,10 +45,7 @@ public class ListGangguanFragment extends Fragment
 
         View root = inflater.inflate(R.layout.gangguan_fragment, container, false);
         myrecyclerview = root.findViewById(R.id.gangguan_recycler);
-        GangguanAdapter gAdapter = new GangguanAdapter(getContext(),dataModels);
-        myrecyclerview.setLayoutManager(new LinearLayoutManager(getActivity()));
-//        myrecyclerview.addItemDecoration(new VerticalSpaceItemDecoration(VERTICAL_ITEM_SPACE));
-        myrecyclerview .setAdapter(gAdapter);
+        init();
 
 
 
@@ -63,27 +64,38 @@ public class ListGangguanFragment extends Fragment
 
         return root;
     }
+    public void init() {
+        try {
+            gAdapter = new GangguanAdapter(getContext(),lsgangguan);
+            myrecyclerview.setLayoutManager(new LinearLayoutManager(getActivity()));
+            myrecyclerview.addItemDecoration(new VerticalSpaceItemDecoration(VERTICAL_ITEM_SPACE));
+            myrecyclerview .setAdapter(gAdapter);
+        } catch (Exception ex) {
+            AjnClass.showAlert(getActivity(), ex.toString());
+        }
+    }
 
     public void def(){
-        dataModels = new ArrayList<>();
+        lsgangguan = new ArrayList<>();
 
         try {
 
             SQLiteHandler DB_Helper = new SQLiteHandler(getActivity());
             SQLiteDatabase db = DB_Helper.getReadableDatabase();
             final Cursor cur = db.rawQuery("SELECT " +
-                    " DISTINCT(ANAKPETAK_ID)" +
+                    " *" +
+//                    " DISTINCT(ANAKPETAK_ID)" +
                     " FROM TRN_GANGGUAN_HUTAN " +
                     " ORDER BY ID DESC", null);
 
             cur.moveToPosition(0);
             dataModels = new ArrayList<>();
             for (int i = 0; i < cur.getCount(); i++) {
-                dataModels.add(new GangguanModel(
+                lsgangguan.add(new GangguanModel(
                         cur.getString(0),
-                        cur.getString(1),
-                        cur.getString(2),
-                        cur.getString(3)));
+                        cur.getString(0),
+                        cur.getString(0),
+                        cur.getString(0)));
                 cur.moveToNext();
             }
 
@@ -97,19 +109,6 @@ public class ListGangguanFragment extends Fragment
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//
         def();
-//        lstGangguan = new ArrayList<>();
-//        lstGangguan.add(new GangguanModel("Penebangan ","Jkt","011","30/11/19"));
-//        lstGangguan.add(new GangguanModel("Penebangan ","Jkt","011","30/11/19"));
-//        lstGangguan.add(new GangguanModel("Penebangan ","Jkt","011","30/11/19"));
-//        lstGangguan.add(new GangguanModel("Penebangan ","Jkt","011","30/11/19"));
-//        lstGangguan.add(new GangguanModel("Penebangan ","Jkt","011","30/11/19"));
-//        lstGangguan.add(new GangguanModel("Penebangan ","Jkt","011","30/11/19"));
-//        lstGangguan.add(new GangguanModel("Penebangan ","Jkt","011","30/11/19"));
-//        lstGangguan.add(new GangguanModel("Penebangan ","Jkt","011","30/11/19"));
-//        lstGangguan.add(new GangguanModel("Penebangan ","Jkt","011","30/11/19"));
-//        lstGangguan.add(new GangguanModel("Penebangan ","Jkt","011","30/11/19"));
-//
     }
 }
