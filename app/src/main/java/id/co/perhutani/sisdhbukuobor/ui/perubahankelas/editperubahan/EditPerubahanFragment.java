58413a07c8d3a392eafbeaf94e397c53d97d245e
Bagus Nurcahyo Putra <bagusnurcahyoputra@gmail.com>
2019-12-04 22:05:54 +0700

    edit spinner id  perubahan kelas

--- app/src/main/java/id/co/perhutani/sisdhbukuobor/ui/perubahankelas/editperubahan/EditPerubahanFragment.java
+++ app/src/main/java/id/co/perhutani/sisdhbukuobor/ui/perubahankelas/editperubahan/EditPerubahanFragment.java
@@ -1,11 +1,16 @@
 package id.co.perhutani.sisdhbukuobor.ui.perubahankelas.editperubahan;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -15,9 +20,14 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
 import id.co.perhutani.sisdhbukuobor.ExtentionClass.AjnClass;
 import id.co.perhutani.sisdhbukuobor.ExtentionClass.SQLiteHandler;
 import id.co.perhutani.sisdhbukuobor.R;
+import id.co.perhutani.sisdhbukuobor.Schema.MstAnakPetakSchema;
 import id.co.perhutani.sisdhbukuobor.Schema.TrnPerubahanKelas;
 import id.co.perhutani.sisdhbukuobor.ui.perubahankelas.ListPerubahanKelasFragment;
 
@@ -31,8 +41,12 @@ public class EditPerubahanFragment extends Fragment {
     private static SQLiteHandler db;
     private static String id, str_petak_id, str_tahun, str_luas, str_jenis_tanaman,
             str_kelas_tanaman, str_luas_perkiraan, str_jenis_perkiraan, str_kelas_perkiraan,
-            str_no_bappkh, str_luas_definitif, str_jenis_definitif, str_kelas_definitif, str_keterangan;
+            str_no_bappkh, str_luas_definitif, str_jenis_definitif, str_kelas_definitif, str_keterangan,
+            str_tanggal;
     private Button btnSimpanPerubahan;
+
+    private Spinner spin_anak_petak;
+    final Calendar calendar = Calendar.getInstance();
 
     private EditPerubahanViewModel mViewModel;
 
@@ -40,10 +54,40 @@ public class EditPerubahanFragment extends Fragment {
         return new EditPerubahanFragment();
     }
 
+    public void load_spinner_anak_petak() {
+        List<String> listtpg = db.getAnakPetak();
+        final int _tpg = listtpg.size();
+        ArrayAdapter<String> dataAdapter_tpg = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_spinner_item, listtpg) {
+            @Override
+            public int getCount() {
+                return (_tpg); // Truncate the list
+            }
+        };
+        dataAdapter_tpg.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin_anak_petak.setAdapter(dataAdapter_tpg);
+        spin_anak_petak.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // your code here
+                String pil_petak = spin_anak_petak.getSelectedItem().toString();
+                String id_petak = db.getDataDetail(MstAnakPetakSchema.TABLE_NAME,
+                        MstAnakPetakSchema.ANAK_PETAK_NAME, pil_petak, MstAnakPetakSchema.ANAK_PETAK_ID);
+                petak_id.setText(id_petak);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+            }
+        });
+    }
+
+    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-//        return inflater.inflate(R.layout.edit_perubahan_fragment, container, false);
+
         View root = inflater.inflate(R.layout.edit_perubahan_fragment, container, false);
 
         db = new SQLiteHandler(getActivity());
@@ -62,6 +106,32 @@ public class EditPerubahanFragment extends Fragment {
 
         petak_id = root.findViewById(R.id.edit_perubahankls_idpetakperubahan);
         tahun = root.findViewById(R.id.edit_perubahankls_tahunperubahan);
+        tahun.setFocusable(false);
+        final DatePickerDialog.OnDateSetListener date1 = new android.app.DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, monthOfYear);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                SimpleDateFormat sdf_view = new SimpleDateFormat("yyyy-MM-dd");
+                str_tanggal = sdf_view.format(calendar.getTime());
+
+                tahun.setText(str_tanggal);
+            }
+
+        };
+        tahun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(getActivity(), date1, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
         luas = root.findViewById(R.id.edit_perubahankls_luasperubahan);
         jenis_tanaman = root.findViewById(R.id.edit_perubahankls_jenisperubahan);
         kelas_tanaman = root.findViewById(R.id.edit_perubahankls_kelasperubahan);
@@ -73,7 +143,13 @@ public class EditPerubahanFragment extends Fragment {
         jenis_definitif = root.findViewById(R.id.edit_perubahankls_jenisdefinitif);
         kelas_definitif = root.findViewById(R.id.edit_perubahankls_kelasdefinitif);
         keterangan = root.findViewById(R.id.edit_perubahankls_ketperubahan);
-        btnSimpanPerubahan = root.findViewById(R.id.edit_perubahankls_btnsubmit);
+        btnSimpanPerubahan = root.findViewById(R.id.edit_perubahankls_btnsimpan);
+
+        spin_anak_petak = root.findViewById(R.id.spinner_anak_petak);
+        load_spinner_anak_petak();
+        String pil_petak = spin_anak_petak.getSelectedItem().toString();
+        String id_petak = db.getDataDetail(MstAnakPetakSchema.TABLE_NAME, MstAnakPetakSchema.ANAK_PETAK_NAME, pil_petak, MstAnakPetakSchema.ANAK_PETAK_ID);
+        petak_id.setText(id_petak);
 
         str_petak_id = db.getDataDetail(TrnPerubahanKelas.TABLE_NAME, TrnPerubahanKelas._ID, id, TrnPerubahanKelas.ANAK_PETAK_ID_PERUBAHAN);
         str_tahun = db.getDataDetail(TrnPerubahanKelas.TABLE_NAME, TrnPerubahanKelas._ID, id, TrnPerubahanKelas.TAHUN_PERUBAHAN);
