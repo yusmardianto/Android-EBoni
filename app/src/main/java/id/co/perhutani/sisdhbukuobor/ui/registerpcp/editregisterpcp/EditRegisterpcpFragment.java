03fd849f23c3b59d3284877f3a0288db48f16be8
Yusmardianto <yusmardianto1@gmail.com>
2019-12-05 19:10:10 +0700

    jancuk

--- app/src/main/java/id/co/perhutani/sisdhbukuobor/ui/registerpcp/editregisterpcp/EditRegisterpcpFragment.java
+++ app/src/main/java/id/co/perhutani/sisdhbukuobor/ui/registerpcp/editregisterpcp/EditRegisterpcpFragment.java
@@ -1,21 +1,38 @@
 package id.co.perhutani.sisdhbukuobor.ui.registerpcp.editregisterpcp;
 
-import androidx.lifecycle.ViewModelProviders;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import java.util.Calendar;
 
+import id.co.perhutani.sisdhbukuobor.ExtentionClass.SQLiteHandler;
 import id.co.perhutani.sisdhbukuobor.R;
 
 public class EditRegisterpcpFragment extends Fragment {
 
+    private EditText nopcp, anakpetak, tahunpcp, luasbaku, luasblok, umur, rataratakeliling,
+            bonita, mpcp, normalpcp, nmati, tahunjurangan, keterangan;
+
+    private static String id, str_nopcp, str_anakpetak, str_tahunpcp, str_luasbaku, str_luasblok,
+            str_umur, str_rataratakeliling, str_bonita, str_mpcp, str_normalpcp, str_nmati, str_tahunjurangan, str_keterangan;
+
+    private Button btnSimpanRegisterpcp;
+
+    private Spinner spin_anak_petak;
+    final Calendar calendar = Calendar.getInstance();
+
+    public static final String MSG_KEY = "id";
+    private static SQLiteHandler db;
     private EditRegisterpcpViewModel mViewModel;
 
     public static EditRegisterpcpFragment newInstance() {
