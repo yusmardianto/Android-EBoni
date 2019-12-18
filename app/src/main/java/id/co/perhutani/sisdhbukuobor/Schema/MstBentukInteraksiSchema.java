784a208e44dd7a0da64d41d8ab9395dbb6441f17
Yusmardianto <yusmardianto1@gmail.com>
2019-12-18 15:06:17 +0700

    add master desa, bentuk interaksi, status

---
+++ app/src/main/java/id/co/perhutani/sisdhbukuobor/Schema/MstBentukInteraksiSchema.java
@@ -0,0 +1,36 @@
+package id.co.perhutani.sisdhbukuobor.Schema;
+
+public class MstBentukInteraksiSchema {
+    public static final String TABLE_NAME = "MST_DESA";
+    public static final String _ID = "ID";
+    public static final String ID_INTERAKSI = "ID_INTERAKSI";
+    public static final String NAMA_INTERAKSI = "NAMA_INTERAKSI";
+    public static final String KET1 = "KET1";
+    public static final String KET2 = "KET2";
+    public static final String KET3 = "KET3";
+    public static final String KET4 = "KET4";
+    public static final String KET5 = "KET5";
+    public static final String KET6 = "KET6";
+    public static final String KET7 = "KET7";
+    public static final String KET8 = "KET8";
+    public static final String KET9 = "KET9";
+    public static final String KET10 = "KET10";
+    public static final String SQL_CREATE_ENTRIES = "CREATE TABLE " +
+            "IF NOT EXISTS "+ TABLE_NAME +" ( "+ _ID +" INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "" + ID_INTERAKSI + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + NAMA_INTERAKSI + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KET1 + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KET2 + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KET3 + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KET4 + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KET5 + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KET6 + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KET7 + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KET8 + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KET9 + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + KET10 + " " + Type.VARCHAR_NULLABLE_100 + ", " +
+            "" + Type.CREATED_AT_CURRENT_DATE + ", " +
+            "" + Type.UPDATED_AT_CURRENT_DATE + " )";
+    public static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    public static final String SQL_DELETE_ALL_ROWS = "DELETE FROM " + TABLE_NAME + " WHERE " + _ID + " <> 0";
+}
