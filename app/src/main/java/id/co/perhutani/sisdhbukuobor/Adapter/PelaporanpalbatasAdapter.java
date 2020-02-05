071bcd965e1af2004c425547480052fa68d6af23
yusmardianto <yus@gmail.com>
2020-02-05 21:17:15 +0700

    fix re-design ui

--- app/src/main/java/id/co/perhutani/sisdhbukuobor/Adapter/PelaporanpalbatasAdapter.java
+++ app/src/main/java/id/co/perhutani/sisdhbukuobor/Adapter/PelaporanpalbatasAdapter.java
@@ -78,14 +78,14 @@ public class PelaporanpalbatasAdapter extends RecyclerView.Adapter<Pelaporanpalb
         });
 
         String status_sync = db.getDataDetail(TrnLaporanPalBatas.TABLE_NAME, TrnLaporanPalBatas._ID, mData.get(position).getID(), TrnLaporanPalBatas.KET9);
-        if (status_sync.equals("1")) {
-            holder.name_data_sinkron.setText("Belum terkirim keserver");
-            holder.name_data_sinkron.setTextColor(Color.rgb(228,0,4));
-            holder.name_info_alert.setBackgroundDrawable(ContextCompat.getDrawable(mContext, R.drawable.ic_info_outline_red_24dp));
-        }  else {
-            holder.name_info_alert.setBackgroundDrawable(ContextCompat.getDrawable(mContext, R.drawable.ic_check_circle_green_24dp));
+        if (status_sync.equals("1")){
             holder.name_data_sinkron.setText("Sudah terkirim keserver");
             holder.name_data_sinkron.setTextColor(Color.rgb(146,198,91));
+            holder.name_info_alert.setBackgroundDrawable(ContextCompat.getDrawable(mContext, R.drawable.ic_check_circle_green_24dp));
+        }  else {
+            holder.name_info_alert.setBackgroundDrawable(ContextCompat.getDrawable(mContext, R.drawable.ic_info_outline_red_24dp));
+            holder.name_data_sinkron.setText("Belum terkirim keserver");
+            holder.name_data_sinkron.setTextColor(Color.rgb(228,0,4));
         }
     }
 
