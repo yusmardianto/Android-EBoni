d321940c35ee6b811e69df1ba80d615865809ea0
yusmardianto <yus@gmail.com>
2020-02-12 03:59:57 +0700

    create api pemantauan

--- app/src/main/java/id/co/perhutani/sisdhbukuobor/Adapter/PemantauansatwaAdapter.java
+++ app/src/main/java/id/co/perhutani/sisdhbukuobor/Adapter/PemantauansatwaAdapter.java
@@ -78,15 +78,16 @@ public class PemantauansatwaAdapter extends RecyclerView.Adapter<Pemantauansatwa
         });
 
         String status_sync = db.getDataDetail(TrnPemantauanSatwa.TABLE_NAME, TrnPemantauanSatwa._ID, mData.get(position).getID(), TrnPemantauanSatwa.KET9);
-        if (status_sync.equals("1")){
+        if (status_sync.equals("1")) {
+            holder.name_info_alert.setBackgroundDrawable(ContextCompat.getDrawable(mContext, R.drawable.ic_check_circle_green_24dp));
             holder.name_data_sinkron.setText("Sudah terkirim keserver");
             holder.name_data_sinkron.setTextColor(Color.rgb(146,198,91));
-            holder.name_info_alert.setBackgroundDrawable(ContextCompat.getDrawable(mContext, R.drawable.ic_check_circle_green_24dp));
         }  else {
-            holder.name_info_alert.setBackgroundDrawable(ContextCompat.getDrawable(mContext, R.drawable.ic_info_outline_red_24dp));
             holder.name_data_sinkron.setText("Belum terkirim keserver");
             holder.name_data_sinkron.setTextColor(Color.rgb(228,0,4));
+            holder.name_info_alert.setBackgroundDrawable(ContextCompat.getDrawable(mContext, R.drawable.ic_info_outline_red_24dp));
         }
+
     }
 
     @Override
